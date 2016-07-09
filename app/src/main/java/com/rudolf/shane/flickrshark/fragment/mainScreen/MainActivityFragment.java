6383253c85ae141928340dbe0c927096af1d0a7e
Shane <shaneRudolfWorktive@gmail.com>
2016-07-09 11:03:59 -0700

    support orientation

--- app/src/main/java/com/rudolf/shane/flickrshark/fragment/mainScreen/MainActivityFragment.java
+++ app/src/main/java/com/rudolf/shane/flickrshark/fragment/mainScreen/MainActivityFragment.java
@@ -1,7 +1,6 @@
 package com.rudolf.shane.flickrshark.fragment.mainScreen;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,21 +8,43 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.rudolf.shane.flickrshark.R;
+import com.rudolf.shane.flickrshark.base.BaseFragment;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends BaseFragment {
+
+    //View States
+    Integer totalChildBeforOrientationChange = null;
+    Integer listPositionBeforOrientationChange = null;
+
+    RecyclerView recyclerView;
+    MainRecycleViewAdapter adapter;
+    GridLayoutManager layoutManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        final RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerViewMainShark);
-        final GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 3);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerViewMainShark);
+         layoutManager = new GridLayoutManager(getActivity(), 3);
         recyclerView.setLayoutManager(layoutManager);
-        final MainRecycleViewAdapter adapter = new MainRecycleViewAdapter(new String[]{});
+        adapter = new MainRecycleViewAdapter(new String[]{});
         recyclerView.setAdapter(adapter);
         recyclerView.addOnScrollListener(new InifiniteScrollListener(layoutManager, adapter));
 
+        //View States
+        if (totalChildBeforOrientationChange != null) adapter.setItemCount(totalChildBeforOrientationChange);
+        if (listPositionBeforOrientationChange != null) recyclerView.offsetChildrenVertical(listPositionBeforOrientationChange);
+
         return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        //Save View States
+        totalChildBeforOrientationChange = adapter.getItemCount();
+        listPositionBeforOrientationChange = recyclerView.computeVerticalScrollOffset();
     }
 }
