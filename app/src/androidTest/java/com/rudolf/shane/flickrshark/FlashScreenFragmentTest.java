75e8b73ad21daea0ececa60557041029c64a3fe8
Shane <shaneRudolfWorktive@gmail.com>
2016-08-06 23:26:54 -0700

    add test

---
+++ app/src/androidTest/java/com/rudolf/shane/flickrshark/FlashScreenFragmentTest.java
@@ -0,0 +1,44 @@
+package com.rudolf.shane.flickrshark;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by shane on 8/6/16.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class FlashScreenFragmentTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    private UiDevice mDevice;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
+    }
+
+    @Test
+    public void testResponseHandlerError() {
+        onView(withId(R.id.textViewUsername)).check(matches(withText("error")));
+    }
+}
